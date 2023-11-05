56b7ddb4a5149f5655cf490140d7283e312e88ad
belab <b3lab@gmx.net>
2023-11-05 22:26:52 +0100

    Simplified sprite handling

--- Sprites.h
+++ Sprites.h
@@ -12,52 +12,18 @@ struct Sprites
 	static constexpr uint8_t SPRITE_ALIGNMENT = 64;
 	static constexpr uint16_t SPRITE_DATA_POINTERS = 2040;
 
-	template <bool multicolor>
-	struct SpriteLine
-	{
-		uint8_t pixels[3];
-
-		typedef uint_least64_t numeric_t;
-
-		/// Pixel format converter.
-		static constexpr uint8_t c(const numeric_t m)
-		{
-			if constexpr (multicolor)
-				// 0b00ii00jj00kk00ll -> 0biijjkkll
-				return uint8_t(m >> 0 & 3 << 0) |
-					   uint8_t(m >> 2 & 3 << 2) |
-					   uint8_t(m >> 4 & 3 << 4) |
-					   uint8_t(m >> 6 & 3 << 6);
-			return uint8_t(m);
-		}
-
-		/// Input bits per byte.
-		constexpr static auto ibb = multicolor ? 16 : 8;
-
-		/// Constructor.
-		constexpr SpriteLine(const numeric_t line)
-			: pixels{c(line >> (2 * ibb)), c(line >> ibb), c(line)}
-		{
-		}
-	};
-
-	template <bool multicolor>
 	struct Sprite
 	{
-		typedef SpriteLine<multicolor> Line;
-		alignas(SPRITE_ALIGNMENT) Line lines[SPRITE_ALIGNMENT / sizeof(Line)];
+		alignas(SPRITE_ALIGNMENT) uint8_t data[SPRITE_ALIGNMENT];
 	};
 
-	typedef Sprite<false> HighResSprite;
-	typedef Sprite<true> MultiColorSprite;
-
-	template <bool multicolor>
 	static void activate(const uint8_t sprite_number,
-						 const Sprite<multicolor> &bitmap,
+						 const Sprite& bitmap,
+						 const bool multicolor,
 						 const bool low_priority,
 						 const bool double_width, const bool double_height)
 	{
-		Utils::memory(SPRITE_DATA_POINTERS + sprite_number) = (std::ptrdiff_t(bitmap.lines) & 0x3fff) / SPRITE_ALIGNMENT;
+		Utils::memory(SPRITE_DATA_POINTERS + sprite_number) = (std::ptrdiff_t(bitmap.data) & 0x3fff) / SPRITE_ALIGNMENT;
 		Utils::setBit(VIC.spr_ena, sprite_number, true);
 		Utils::setBit(VIC.spr_exp_x, sprite_number, double_width);
 		Utils::setBit(VIC.spr_exp_y, sprite_number, double_height);
